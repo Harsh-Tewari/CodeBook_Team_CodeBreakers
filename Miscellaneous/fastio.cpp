ec16f9264f85883820832154c3ee4d131c383859
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2023-03-31 23:53:58 +0530

    fastio

---
+++ Miscellaneous/fastio.cpp
@@ -0,0 +1,12 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+const long double Pi = 3.141592653;
+#define all(x) x.begin(), x.end()
+#define precise(x) cout << fixed << setprecision(10) << x << endl;
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
